fdfbd19f295f77210d70e0bb94b7ca1d374d677f
Nikolajs Katane <nickkata24@gmail.com>
2025-02-22 14:03:13 +0200

    second version

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Darbs uz atzīmi 20.02.2025
+# Auto Foto Lapa
+
+Šī lapa piedāvā kolekciju augstas kvalitātes automašīnu fotogrāfijām. Šeit varat izbaudīt attēlus no elegantiem sporta automobiļiem līdz ikdienas klasikai no visas pasaules.
+
+## Galvenās funkcijas
+
+- **HD un profesionālas fotogrāfijas**: Izbaudiet skaidrus un detalizētus attēlus.
+- **Ikoniskie modeļi un jaunākās tendences**: Atklājiet jaunākās tendences auto pasaulē.
+- **Ideāli faniem un dizaina entuziastiem**: Ideāli piemērots tiem, kas novērtē stilu un dizainu.
+
+## Kā lietot
+
+Vienkārši apmeklējiet galveno lapu, lai apskatītu skaistas auto fotogrāfijas. Jūs varat mainīt attēlus, nospiežot pogas vai vienkārši ritinot lapu.
+
+## Tehnoloģijas
+
+- HTML, CSS
+- JavaScript
--- src/js/script.js
+++ src/js/script.js
@@ -1,15 +1,22 @@
-let count = 0;
-function increaseCounter() {
-    count++;
-    document.getElementById("counter").innerText = count;
+let images = [
+    "/src/images/car2.jpg",
+    "/src/images/car3.jpg",
+    "/src/images/car4.jpg",
+    "/src/images/car1.jpg"
+];
+let currentIndex = 0;
+
+function changeImage() {
+    currentIndex = (currentIndex + 1) % images.length;
+    document.getElementById("image").src = images[currentIndex];
 }
+
 
 async function getCatFact() {
     try {
-        let response = await fetch("https://catfact.ninja/fact");
-        let data = await response.json();
-        document.getElementById("cat-fact").innerText = data.fact;
-    } catch (error) {
+        let { fact } = await (await fetch("https://catfact.ninja/fact")).json();
+        document.getElementById("cat-fact").innerText = fact;
+    } catch {
         document.getElementById("cat-fact").innerText = "Loading failed";
     }
 }
